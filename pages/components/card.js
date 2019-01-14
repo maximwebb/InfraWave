236a3fca8c416270120f3c582666ab6c54c718e0
Maxim Webb <maximwebb@gmail.com>
2019-01-14 23:38:38 +0000

    Fix broken hyperlinks on songs page

--- pages/components/card.js
+++ pages/components/card.js
@@ -1,21 +1,34 @@
 import React from 'react'
+import Link from 'next/link'
 
 const IMG = (props) => (
-	<div className="card s6 m4 l4 xl3 blue-grey song-card" href={props.link}>
-		{props.image &&
-			<div className={"card-image"}>
-				 <img src={props.image} />
-				 <h4 className={"card-title"}> {props.title} </h4>
-			</div>
-		}
-		<div className="card-content">
-			{!props.image &&
-				<span className="card-title">{props.title}</span>
+	<Link as={props.link} href={props.link}>
+		<a>
+		<div className="card s6 m4 l4 xl3 blue-grey song-card" href={props.link}>
+			{props.image &&
+				<div className={"card-image"}>
+					 <img src={props.image} />
+					 <h4 className={"card-title"}> {props.title} </h4>
+				</div>
 			}
-			<p>By {props.artist}</p>
+			<div className="card-content">
+				{!props.image &&
+					<span className="card-title">{props.title}</span>
+				}
+				<p>By {props.artist}</p>
+			</div>
 		</div>
+		</a>
+	</Link>
+);
 
+const PostLink = (props) => (
+	<div>
+		<Link as={props.title} href={props.title}>
+			<a>{props.title}</a>
+		</Link>
 	</div>
 );
+
 
 export default IMG
