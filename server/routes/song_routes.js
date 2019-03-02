0a0707aa7ae61badf3475d47630e966863ccb462
Maxim Webb <maximwebb@gmail.com>
2019-03-02 20:15:29 +0000

    Add UPDATE and DELETE routes to songs API (song_routes) #6

--- server/routes/song_routes.js
+++ server/routes/song_routes.js
@@ -13,8 +13,12 @@ module.exports = function(app, db) {
 	});
 
 	app.post('/server/add_song', (req, res) => {
-		const song = { 'title': req.body.title, 'artist': req.body.artist, 'link': req.body.link, 'duration': req.body.duration, 'thumbnail': req.body.thumbnail };
-		console.log(song);
+		const song = { 'title': req.body.title,
+			'artist': req.body.artist,
+			'link': req.body.link,
+			'duration': req.body.duration,
+			'thumbnail': req.body.thumbnail };
+
 		db.collection('songs').insertOne(song, (err, result) => {
 			if (err) {
 				res.send({ 'error': err });
@@ -22,6 +26,44 @@ module.exports = function(app, db) {
 			else {
 				res.send('Song successfully added.');
 			}
-		})
+		});
 	});
+
+	app.put('/server/update_song/:id', (req, res) => {
+		const id = req.params.id;
+		const details = { '_id': new ObjectID(id) };
+
+		let song = {};
+		let parameters = ['title', 'artist', 'link', 'duration', 'thumbnail'];
+		for (let i of Object.keys(req.body)) {
+			//Checks if keys in search request are valid, and constructs song object if so.
+			if (parameters.find((param) => { return i === param })) {
+				song[i] = req.body[i];
+			}
+		}
+
+		db.collection('songs').updateOne(details, { $set: song }, (err, result) => {
+			if (err) {
+				res.send(err);
+			}
+			else {
+				res.send('Song successfully updated');
+			}
+		});
+	});
+
+	app.delete('/server/delete_song/:id', (req, res) => {
+		const id = req.params.id;
+		const name = req.query.name;
+		const details = { '_id': new ObjectID(id) };
+
+		db.collection('songs').deleteOne(details, (err, result) => {
+			if (err) {
+				res.send(err);
+			}
+			else {
+				res.send(`Song successfully deleted.`);
+			}
+		});
+	})
 };
